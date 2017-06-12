ef569fb8bc5fdb3caa8e876aacbbd7c06e29f20e
Unknown <d.brioude@gmail.com>
2017-06-12 14:44:46 +0200

    Correction classe Jeu

--- src/model/Jeu.java
+++ src/model/Jeu.java
@@ -2,7 +2,6 @@ package model;
 
 import tools.ChessPiecesFactory;
 
-import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,32 +10,27 @@ import java.util.List;
 public class Jeu implements Game {
 
     private List<Pieces> pieces;
-    private Couleur couleur;
-    boolean castling = false;
 
     public Jeu(Couleur couleur) {
         pieces = ChessPiecesFactory.newPieces(couleur);
-        this.couleur = couleur;
     }
 
     @Override
     public boolean isPieceHere(int x, int y) {
-        return findPiece(x,y)!=null;
-    }
-
-    @Override
-    public boolean isMoveOk(int xInit, int yInit, int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible) {
-        Pieces piece = findPiece(xInit,yInit);
-        if(piece != null)
-            return piece.isMoveOk(xFinal, yFinal,isCatchOk,isCastlingPossible);
+        for(Pieces piece:pieces){
+            if(piece.getX()==x && piece.getY() == y)
+                return true;
+        }
         return false;
     }
 
     @Override
+    public boolean isMoveOk(int xInit, int yInit, int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible) {
+        return  false;
+    }
+
+    @Override
     public boolean move(int xInit, int yInit, int xFinal, int yFinal) {
-        Pieces piece = findPiece(xInit,yInit);
-        if(piece != null)
-            return piece.move(xFinal,yFinal);
         return false;
     }
 
@@ -52,55 +46,5 @@ public class Jeu implements Game {
             res+=piece;
         }
         return res;
-    }
-
-    public Couleur getCouleur(){
-        return couleur;
-    }
-
-    public Couleur getPieceColor(int x, int y){
-        Pieces piece = findPiece(x,y);
-        if(piece != null)
-            return piece.getCouleur();
-        return null;
-    }
-
-    public String getPieceName(int x, int y){
-        Pieces piece = findPiece(x,y);
-        if(piece != null)
-            return piece.getName();
-        return "";
-    }
-
-    public List<PieceIHMs> getPiecesIHM(){
-        PieceIHMs newPieceIHM = null;
-        List<PieceIHMs> list = new LinkedList<PieceIHMs>();
-        for (Pieces piece : pieces){
-        // si la pièce est toujours en jeu
-            if (piece.getX() != -1){
-                newPieceIHM = new PieceIHM(piece);
-                list.add(newPieceIHM);
-            }
-        }
-        return list;
-    }
-
-    public void setCastling(){
-        castling = !castling;
-    }
-
-    private Pieces findPiece(int x, int y){
-        for(Pieces piece : pieces){
-            if(piece.getY() == y && piece.getX()==x)
-                return piece;
-        }
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Jeu jeu = new Jeu(Couleur.NOIR);
-        System.out.println(jeu);
-        System.out.println(jeu.findPiece(7,0));
-        System.out.println(jeu.isMoveOk(1,0,3,1,false,false));
     }
 }
