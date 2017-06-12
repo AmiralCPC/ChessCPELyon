2f3c54b6cba7ab0ed519f5c65b00b89d8204ba47
Unknown <d.brioude@gmail.com>
2017-06-12 09:26:08 +0200

    Création de l'interface Pieces

---
+++ src/model/Pieces/Pieces.java
@@ -0,0 +1,16 @@
+package model.Pieces;
+
+import model.Couleur;
+
+/**
+ * Created by Damien on 12/06/2017.
+ */
+public interface Pieces {
+    public boolean capture();
+    public Couleur getCouleur();
+    public String getName();
+    public int getX();
+    public int getY();
+    public boolean isMoveOk(int xFinal, int yFinal, boolean isCatchOk, boolean isCastlingPossible);
+    public boolean move(int xFinal, int yFinal);
+}
