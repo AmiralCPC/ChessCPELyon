caf0715d60939ae329f4e340b06211fc29151c77
Unknown <d.brioude@gmail.com>
2017-06-12 09:40:20 +0200

    Création abstractPiece pour de vrai

--- src/model/Pieces/AbstractPiece.java
+++ src/model/Pieces/AbstractPiece.java
@@ -1,7 +1,31 @@
 package model.Pieces;
 
+import model.Coord;
+import model.Couleur;
+
 /**
  * Created by Damien on 12/06/2017.
  */
-public class AbstractPiece {
+public abstract class AbstractPiece implements Pieces{
+
+    private Couleur couleur;
+    private Coord coord;
+
+    AbstractPiece(Couleur couleur, Coord coord){
+        this.couleur = couleur;
+        this.coord = coord;
+    }
+
+    public Couleur getCouleur() {
+        return couleur;
+    }
+
+    public int getX() {
+        return coord.x;
+    }
+
+    public int getY() {
+        return coord.y;
+    }
+
 }
